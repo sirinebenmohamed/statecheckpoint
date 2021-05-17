27c486ec489614d6e23044cadfee186d15eeb7e4
sirine ben mohamed <sirinebenmohamed777@gmail.com>
2021-05-17 11:56:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+
+ class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+        fullName:'Syrine Ben Mohamed'
+        , imgSrc:'https://img.icons8.com/pastel-glyph/2x/person-male.png', profession:'Full stuck JS Student',
+        show:true,count:0}
 }
+
+componentDidMount(){
+    this.timer = setInterval(this.countFunc, 1000);
+}
+countFunc=()=>{
+    this.setState({...this.state,count:this.state.count+1})
+}
+
+  render() {
+    return (
+      <div>
+      <button onClick={()=>this.setState({...this.state,show:!this.state.show})}>{this.state.show===true?'hide':'show'}</button>    
+      <p>Timer = {this.state.count}</p>
+      {this.state.show===true?<div><h1>{this.state.fullName}</h1>
+      <h1>{this.state.profession}</h1>
+      <img src={this.state.imgSrc} alt="" /></div>:<div></div>}
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <Profile/>
+//     </div>
+//   );
+//}
 
 export default App;
---
+++ src/Profile.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            fullName:'Syrine Ben Mohamed'
+            , imgSrc:'https://img.icons8.com/pastel-glyph/2x/person-male.png', profession:'Full stuck JS Student',
+            show:true,count:0}
+    }
+   
+    componentDidMount(){
+        this.timer = setInterval(this.countFunc, 1000);
+    }
+    countFunc=()=>{
+        this.setState({...this.state,count:this.state.count+1})
+    }
+    render() {
+        return (
+            <div>
+            <button onClick={()=>this.setState({...this.state,show:!this.state.show})}>{this.state.show===true?'hide':'show'}</button>    
+            <p>Timer = {this.state.count}</p>
+            {this.state.show===true?<div><h1>{this.state.fullName}</h1>
+            <h1>{this.state.profession}</h1>
+            <img src={this.state.imgSrc} alt="" /></div>:<div></div>}
+            </div>
+        )
+    }
+}
